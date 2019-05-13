e3c2beb37527e0fd06f0969625b8b9f7f80e8faa
Marcos Vinicius <33167998+bboats@users.noreply.github.com>
2019-05-12 23:32:59 -0300

    OOP'ization part 2 electric boogaloo

--- bin/main.py
+++ bin/main.py
@@ -12,26 +12,6 @@ automataName,language = lines[0].split('=')  ### original format is "automataNam
 #lines[1] has no purpose other than formatting so it is useless to this program
 AFNoperations = lines[2:]
 afn = Afn(automataName,language,AFNoperations)
-#Formats the language so it can be easily split later
-#language = formatLanguage(language)
-#states,transitions,initialState,finalStates = language.split(",,")
 
-
-
-
-#Turns all 4 into lists using comma as the separator
-#states = states.split(',')
-#transitions = transitions.split(',')
-#initialState = initialState.split(',')
-#finalStates = finalStates.split(',')
-
-
-
-#AFNoperations = formatOperations(AFNoperations)
-
-#Determinization of the given automata
-#AFDoperations = determinizeOperations(AFNoperations,states,transitions)
 
 ##/\continue from here, determinizeOperations() needs to be made
-
-#print(AFNoperations)
